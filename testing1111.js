0bad4a56dff1daa19b8ff85e09af3926b81a7346
abc <ebookcode123@gmail.com>
2016-12-13 14:38:08 -0800

    new update

--- testing1111.js
+++ testing1111.js
@@ -2,14 +2,24 @@
  * Created by leizha on 12/12/16.
  */
 
-console.log(s);
-ss();
+//console.log(s);
+//ss();
+//
+//
+//var s = 5;
+//
+//var ss = function () {
+//    console.log("hello word")
+//}
 
 
-var s = 5;
-
-var ss = function () {
-    console.log("hello word")
+function a() {
+    function b() {
+        console.log("----" + myVar);
+    }
+    var myVar =2
+    b();
 }
 
-
+var myVar =1;
+a();
