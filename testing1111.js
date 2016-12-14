2384784b9ff9ce8fe1993bde459b7d76a341cae2
abc <ebookcode123@gmail.com>
2016-12-14 13:41:32 -0800

    练习最新上传

--- testing1111.js
+++ testing1111.js
@@ -25,20 +25,27 @@
 // a();
 
 
-function waitTreeseconds() {
-    var ms = 3000 + new Date().getTime();
-    console.log(new Date().getTime());
-    while(new Date() < ms) {
-        console.log('finshi function');
-    }
-}
-
-function clickHandler() {
-    console.log('click event');
-}
+//function waitTreeseconds() {
+//    var ms = 3000 + new Date().getTime();
+//    console.log(new Date().getTime());
+//    while(new Date() < ms) {
+//        console.log('finshi function');
+//    }
+//}
+//
+//function clickHandler() {
+//    console.log('click event');
+//}
 
 //listen for the click event
-document.addEventListener('click',clickHandler);
+//document.addEventListener('click',clickHandler);
 
-waitTreeseconds();
+//waitTreeseconds();
 console.log('finished exexution');
+
+function great(){
+    console.log("abs");
+}
+great.language = "english";
+
+console.log(great);
