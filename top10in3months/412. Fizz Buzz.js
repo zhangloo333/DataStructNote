cd9d94e3ae26d3282d3687428dab9820a59d8056
ABC <zhangloo333@gmail.com>
2017-02-05 23:06:25 -0800

    FIZZBUZZ
    Signed-off-by: ABC <zhangloo333@gmail.com>

--- top10in3months/412. Fizz Buzz.js
+++ top10in3months/412. Fizz Buzz.js
@@ -1,3 +1,24 @@
 /**
  * Created by leizha on 1/31/17.
+ * fizeebuzz
  */
+
+function fizzbuzz(n) {
+    if(n == null && n.length ==0){
+        return -1;
+    }
+    var nNum = parseInt(n);
+    for(var i = 1; i <= n; i++) {
+        if(i%3 == 0 && i%5 == 0){
+            console.log("FizzBuzz");
+        } else if (i%5 == 0) {
+            console.log("buzz");
+        } else if(i%3 == 0) {
+            console.log("Fizee");
+        } else {
+            console.log(""+i);
+        }
+    }
+}
+
+fizzbuzz("15");
