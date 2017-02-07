7a98a808981cb7cb3143df8e66ddc2b4a980a6eb
ebookcode123 <ebookcode123@gmail.com>
2017-02-06 19:28:39 -0800

    fizz buzz add return a tray

--- top10in3months/412. Fizz Buzz.js
+++ top10in3months/412. Fizz Buzz.js
@@ -22,3 +22,25 @@ function fizzbuzz(n) {
 }
 
 fizzbuzz("15");
+
+function fizzbuzz1(n) {
+    if(n == null && n.length ==0){
+        return -1;
+    }
+    var output = [];
+    var nNum = parseInt(n);
+    for(var i = 1; i <= n; i++) {
+        if(i%3 == 0 && i%5 == 0){
+            output.push("FizzBuzz");
+        } else if (i%5 == 0) {
+            output.push("Buzz");
+        } else if(i%3 == 0) {
+            output.push("Fizz");
+        } else {
+            output.push(i.toString());
+        }
+    }
+    return output;
+}
+
+console.log(fizzbuzz1("15"));
