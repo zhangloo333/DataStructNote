c1ed6dd49aecfa32bffb1704e9c0f1079b8050c7
abc <ebookcode123@gmail.com>
2017-02-07 21:58:34 -0800

    add annotation for minimum genetic mutation

--- top10in3months/433. Minimum Genetic Mutation.js
+++ top10in3months/433. Minimum Genetic Mutation.js
@@ -1,15 +1,16 @@
 /**
  * Created by leizha on 1/31/17.
+ 这个题是关于 BFS 的题，是关于图的题，需要先把图学好再来进行这个。
  */
 
 function minMutation(start, end, bank) {
-    if(start == null || end == null || start.length != end.length){
+    if (start == null || end == null || start.length != end.length) {
         return -1;
     }
 
     var bankSet = new Set(bank);
 
-    if(!bankSet.has(end)){
+    if (!bankSet.has(end)) {
         return -1;
     }
 
@@ -19,27 +20,27 @@ function minMutation(start, end, bank) {
     var che = end.split("");
     var level = 0;
     var min = Math.MAX_VALUE;
-    var genes = ['A','C','G','T'];
+    var genes = ['A', 'C', 'G', 'T'];
 
-    while(!(q.length == 0)){
+    while (!(q.length == 0)) {
         var temp = q.shift();
-        if(temp == "#"){
+        if (temp == "#") {
             level++;
-            if(!(q.length == 0)) {
+            if (!(q.length == 0)) {
                 q.unshift("#");
             }
-        } else if(temp == end) {
-            min = Math.min(min,level);
+        } else if (temp == end) {
+            min = Math.min(min, level);
         } else {
             var chs = temp.split("");
-            for(var i = 0; i<chs.length;i++){
-                if(chs[i] != che[i]) {
+            for (var i = 0; i < chs.length; i++) {
+                if (chs[i] != che[i]) {
                     var c = chs[i];
-                    genes.map(function (x) {
-                        if(x != c){
+                    genes.map(function(x) {
+                        if (x != c) {
                             chs[i] = x;
                             var ns = chs[i].join("");
-                            if(bankSet.has(ns)){
+                            if (bankSet.has(ns)) {
                                 q.shift(ns);
                             }
                         }
@@ -51,7 +52,7 @@ function minMutation(start, end, bank) {
 
     }
 
-return min == Math.MAX_VALUE ? -1:min;
+    return min == Math.MAX_VALUE ? -1 : min;
 
 }
 
@@ -59,4 +60,4 @@ var start = "AAAAACCC";
 var end = "AACCCCCC";
 var bank = ["AAAACCCC", "AAACCCCC", "AACCCCCC"];
 
-console.log(minMutation(start,end,bank));
+console.log(minMutation(start, end, bank));
