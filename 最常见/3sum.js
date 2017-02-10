805c7f87cfb5c81fd59df5d539e1899fba47e129
abc <ebookcode123@gmail.com>
2017-02-10 11:33:23 -0800

    修改了 start++ end ++ 的位置，也同时 是前面对比还是和后面对比

--- 最常见/3sum.js
+++ 最常见/3sum.js
@@ -55,10 +55,10 @@ function threeSume(arr,target) {
                 res.push(temp);
                 j++;
                 k--;
-                while(j<k && arr[j] == arr[j-1]){
+                while(j<k && arr[j] == arr[j+1]){
                     j++;
                 }
-                while(j<k && arr[k] == arr[k+1]){
+                while(j<k && arr[k] == arr[k-1]){
                     k--;
                 }
             } else if(arr[j] + arr[k] > sum){
---
+++ 最常见/practice.js
@@ -0,0 +1,38 @@
+/**
+ * Created by lee on 2/9/17.
+ */
+
+
+
+function pr(mes) {
+    process.stdout.write(mes+" ");
+
+}
+
+
+function print(arr) {
+
+    var len = array.length;
+
+    var slower = 0;
+    var faster = 0;
+
+    while(slower){
+
+    }
+
+
+
+    //for(var i = 0; i < len; i++) {
+    //    pr(array[i]);
+    //
+    //    var start = i+1;
+    //    var end = len-1;
+    //    while(start < end) {
+    //    }
+    //}
+}
+
+ var nums = [1,1,1,0];
+
+print(nums);
